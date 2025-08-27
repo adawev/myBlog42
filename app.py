e7754030ed4acf64bf5e2dfed84273f0ec54b04a
Diyor Adashev <adashevdiyor443@gmail.com>
2025-08-27 13:08:05 +0500

    login page

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import os
 from flask import Flask, render_template, session, request, make_response
-
+import hashlib
 from article import Article
 
 app = Flask(__name__)
 
-@app.route('/set-session')
-def set_session():
-    session['user_id'] = 1
-    return 'session set'
+# @app.route('/set-session')
+# def set_session():
+#     session['user_id'] = 1
+#     return 'session set'
+#
+# @app.route('/get-session')
+# def get_session():
+#     return f'user_id={session.get("user_id")}'
 
-@app.route('/get-session')
-def get_session():
-    return f'user_id={session.get("user_id")}'
-
+users={
+    'admin' : '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
+}
 @app.route('/first-time')
 def first_time():
     if 'seen' not in request.cookies:
@@ -37,5 +40,19 @@ def article(slug: str):
     article = articles[slug]
     return render_template('article.html', article=article)
 
+@app.route('/admin')
+def admin():
+    return render_template('login.html')
+
+@app.post('/admin')
+def admin_login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username not in users:
+        return render_template('login.html', error='password/login xato')
+
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    return render_template('login.html', error='hali tayyor emas')
 if __name__ == '__main__':
     app.run(port=4200, debug=True)
